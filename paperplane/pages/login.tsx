5ae3e29ee88755541921ce84f456695f85a53afc
EUNA <beni1026@naver.com>
2022-11-03 03:44:58 +0900

    feat: 홈페이지,로그인 페이지 추가

---
+++ paperplane/pages/login.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export default function login() {
+  return (
+    <>
+      <div className="container">
+        <div>
+          로그인 페이지
+        </div>
+      </div>
+      <style jsx>{`
+        .container {
+          
+        }
+        
+        `}</style>
+    </>
+  )
+}
