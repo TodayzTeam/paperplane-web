49af793e7b9f125fcd570a1ec1439d18edab9e1e
EUNA <beni1026@naver.com>
2022-11-03 03:44:34 +0900

    feat: 헤더, 푸터 추가

---
+++ paperplane/components/navigation/Header.tsx
@@ -0,0 +1,75 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import icon from "./icon.png";
+
+const Header = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const router = useRouter();
+  const path = router.pathname;
+  const className = (link: string) => {
+    return path == link && "active";
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <div>
+          <Link href="/">
+            <div className="logo">종이비행기</div>
+          </Link>
+          {isLogin ? <div></div> : ""}
+        </div>
+        <div>
+          {isLogin ? 
+          <div className="account-box">
+            <Image src={icon} alt="icon"/>
+            <Link href="/">
+              <div className="logout" onClick={() => {setIsLogin(false)}}>로그아웃</div>
+            </Link>
+            <Link href="/profile">
+              <div className="profile">내정보</div>
+            </Link>
+          </div>
+          :<div className="account-box">
+            <Link href="/login">
+              <div className="login" onClick={() => {setIsLogin(true)}}>로그인</div>
+            </Link>
+            <Link href="/signup">
+              <div className="signup">회원가입</div>
+            </Link>
+          </div>
+          }
+          
+        </div>
+      </div>
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
+          height: 60px;
+          background-color: white;
+          padding: 0 5%;
+        }
+        .logo {
+          font-size: 28px;
+          font-weight: 700;
+          color: #6B85A4;
+        }
+        .account-box {
+          display: flex;
+          flex-direction: row;
+          font-size: 13px;
+          font-weight: 400;
+          gap: 40px;
+        }
+      `}</style>
+    </header>
+  );
+};
+
+export default Header;
