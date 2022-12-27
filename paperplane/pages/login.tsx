f1e294559e6c7a549bee8252ad5ce4bb6a680cb3
EUNA <beni1026@naver.com>
2022-12-27 17:31:48 +0900

    feat: redux login state 추가

--- paperplane/pages/login.tsx
+++ paperplane/pages/login.tsx
@@ -1,19 +1,33 @@
-import { useState } from "react"
+import { useState, useEffect, useCallback } from 'react';
+import { useRouter } from 'next/router';
+import KaKao from '../components/login/KaKao';
+import { useDispatch, useSelector } from 'react-redux';
+import * as loginActions from '../store/modules/login';
+const URL = 'https://a835-222-120-89-55.jp.ngrok.io';
 
 export default function login() {
+  const dispatch = useDispatch();
+  const value = useSelector(({ login }) => login.value);
+
+  const login = useCallback(() => {
+    dispatch(loginActions.login());
+  }, [dispatch]);
+
+  const logout = useCallback(() => {
+    dispatch(loginActions.logout());
+  }, [dispatch]);
+
   return (
     <>
       <div className="container">
-        <div>
-          로그인 페이지
-        </div>
+        <button onClick={() => login()}>로그인</button>
+        <button onClick={() => logout()}>로그아웃</button>
+        <div>{/* <KaKao href={`${URL}/oauth2/authorization/kakao`} /> */}</div>
       </div>
       <style jsx>{`
         .container {
-          
         }
-        
-        `}</style>
+      `}</style>
     </>
-  )
+  );
 }
