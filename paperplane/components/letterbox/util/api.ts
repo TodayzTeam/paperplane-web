e82776680dca157a680c4972fd3f3b5b1924fc2c
EUNA <beni1026@naver.com>
2023-01-26 23:56:14 +0900

    fix: api호출 try catch적용

--- paperplane/components/letterbox/util/api.ts
+++ paperplane/components/letterbox/util/api.ts
@@ -2,27 +2,47 @@ import { publicApi } from '../../../util/api';
 import { letter } from './types';
 
 export async function getReceivedPost(page: number) {
-  const res = await publicApi.get(`/post/received?page=${page}`);
-  return res.data;
+  try {
+    const res = await publicApi.get(`/post/received?page=${page}`);
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function getSentPost(page: number) {
-  const res = await publicApi.get(`/post/sent/random?page=${page}`);
-  return res.data;
+  try {
+    const res = await publicApi.get(`/post/sent/random?page=${page}`);
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function getReadedPosts(page: number) {
-  const res = await publicApi.get(`/post/received/read?page=${page}`);
+  try {
+    const res = await publicApi.get(`/post/received/read?page=${page}`);
 
-  return res.data;
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function getGroupSentPost(page: number) {
-  const res = await publicApi.get(`/post/sent/group?page=${page}`);
-  return res.data;
+  try {
+    const res = await publicApi.get(`/post/sent/group?page=${page}`);
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function getLikedPost(page: number) {
-  const res = await publicApi.get(`/post/liked?page=${page}`);
-  return res.data;
+  try {
+    const res = await publicApi.get(`/post/liked?page=${page}`);
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
